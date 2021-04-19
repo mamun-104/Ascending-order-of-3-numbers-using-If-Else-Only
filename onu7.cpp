d625579762884d776cbfb4dd768d49f5e8001dbf
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-04-20 02:13:10 +0600

    Add files via upload

---
+++ onu7.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<math.h>
+
+/// Ascending order of 3 numbers using If-Else Only
+int main()
+{
+   int num1,num2,num3;
+   scanf("%d%d%d",&num1,&num2,&num3);
+
+   if(num1>num2 && num1>num3)
+   {
+       if(num2>num3)
+       {
+           printf("%d %d %d",num3,num2,num1);
+       }
+       else
+       {
+           printf("%d %d %d",num2,num3,num1);
+       }
+   }
+   else if(num2>num1 && num2>num3)
+   {
+       if(num1>num3)
+       {
+           printf("%d %d %d",num3,num1,num2);
+       }
+       else
+       {
+           printf("%d %d %d",num1,num3,num2);
+       }
+   }
+   else if(num3>num1 && num3>num1)
+   {
+       if(num1>num2)
+       {
+           printf("%d %d %d",num2,num1,num3);
+       }
+       else
+       {
+           printf("%d %d %d",num1,num2,num3);
+       }
+   }
+
+
+
+   return 0;
+}
+
